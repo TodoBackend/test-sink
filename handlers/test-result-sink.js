6e0b372c8aa0f2795be02299ea5b8eca2c69031a
Pete Hodgson <git@thepete.net>
2019-09-24 22:06:21 -0700

    storing test results in S3

--- handlers/test-result-sink.js
+++ handlers/test-result-sink.js
@@ -1,12 +1,16 @@
 'use strict';
 const AWS = require('aws-sdk');
+const makeUuid = require('uuid/v4');
 
 const BUCKET_NAME = process.env.LAKE_BUCKET;
 const s3 = new AWS.S3();
 
 module.exports.handler = async event => {
-    const key = 'testy/test';
-    const body = "WE DID IT";
+    const uuid = makeUuid();
+    const key = `test-results/${uuid}`;
+    const body = event.body;
+
+    console.log('writing test results to',BUCKET_NAME,key);
 
     const result = await s3.putObject({
         Bucket: BUCKET_NAME,
@@ -17,7 +21,7 @@ module.exports.handler = async event => {
     console.log('S3 put result:', result);
 
     return {
-        statusCode: 200,
-        body: "WE PUT STUFF IN S3!"
+        statusCode: 201,
+        body: uuid
     };
 };

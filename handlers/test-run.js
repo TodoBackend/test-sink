2e0767efd896894630145b50b25a1d00020a9ddb
Pete Hodgson <git@thepete.net>
2019-09-30 23:17:54 -0700

    fix up honeycomb instrumentation

--- handlers/test-run.js
+++ handlers/test-run.js
@@ -1,12 +1,12 @@
 'use strict';
-const Libhoney = require('libhoney');
+const beeline = require("honeycomb-beeline")({
+  writeKey: process.env.HONEYCOMB_API_KEY,
+  dataset: process.env.HONEYCOMB_DATASET,
+  serviceName: "test-sink"
+});
+
 const AWS = require('aws-sdk');
 const makeUuid = require('uuid/v4');
-
-let honeycomb = new Libhoney({
-    writeKey: process.env.HONEYCOMB_API_KEY,
-    dataset: process.env.HONEYCOMB_DATASET
-  });
 
 const BUCKET_NAME = process.env.LAKE_BUCKET;
 const TABLE_NAME = process.env.TEST_RESULTS_TABLE;
@@ -14,81 +14,81 @@ const TABLE_NAME = process.env.TEST_RESULTS_TABLE;
 const s3 = new AWS.S3();
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-module.exports.create = async (event, context) => {
-    const startTime = Date.now();
+function withTracePromise(metadataContext, fn, withTraceId, withParentSpanId, withDataset) {
+    const trace = beeline.startTrace(metadataContext, withTraceId, withParentSpanId, withDataset);
+    return fn().finally( ()=>{
+      beeline.finishTrace(trace);
+    } );
+}
 
-    const honeycombEvent = honeycomb.newEvent();
-    honeycombEvent.add({
+module.exports.create = (event, context) => {
+    const ctx = beeline.unmarshalTraceContext((event.headers||{})["x-honeycomb-trace"] || "") || {};
+    return withTracePromise({
+        name: "testRunCreate",
         functionName: context.functionName,
         functionVersion: context.functionVersion,
-        requestId: context.awsRequestId,
-    });
+        requestId: context.awsRequestId
+      }, async () => {
+        const testRunId = makeUuid();
+        const createdAt = new Date().toISOString();
+        beeline.addContext({testRunId});
 
-    const testRunId = makeUuid();
-    const createdAt = new Date().toISOString();
-    honeycombEvent.addField('testRunId', testRunId);
+        await createTestRunInDb({uid:testRunId,createdAt});
 
-    await createTestRunInDb({uid:testRunId,createdAt});
+        const baseUrl = event.requestContext.path;
+        const testRunUrl = `${baseUrl}/${testRunId}`;
+        const testResultsUrl = `${testRunUrl}/results`;
 
-    const baseUrl = event.requestContext.path;
-    const testRunUrl = `${baseUrl}/${testRunId}`;
-    const testResultsUrl = `${testRunUrl}/results`;
+        const response = {
+            _links: {
+                self: { href: testRunUrl },
+                results: { href: testResultsUrl }
+            }
+        };
 
-    const response = {
-        _links: {
-            self: { href: testRunUrl },
-            results: { href: testResultsUrl }
+        return {
+            statusCode: 201,
+            headers: {
+                "Location": testRunUrl,
+                "Content-Type": "application/json;charset=utf-8",
+                "Access-Control-Allow-Origin": "*" // TODO: lock down to todobackend.com
+            },
+            body: JSON.stringify(response)
+        };
+      },
+      ctx.traceId, ctx.parentSpanId);
+};
+
+module.exports.recordResults = (event,context) => {
+    const ctx = beeline.unmarshalTraceContext((event.headers||{})["x-honeycomb-trace"] || "") || {};
+    return withTracePromise({
+        name: "testResultPost",
+        functionName: context.functionName,
+        functionVersion: context.functionVersion,
+        requestId: context.awsRequestId
+      }, async () => {
+        const testRunId = event.pathParameters.testRunId;
+        const completedAt = new Date().toISOString();
+
+        const testResults = event.body; // TODO: validation, DoS protection...
+        
+        beeline.addContext({testRunId});
+        // TODO: add full duration of test runs
+
+        await Promise.all([
+            recordRunCompletionInDb({uid:testRunId,completedAt}),
+            writeResultsToS3(testRunId,testResults)
+        ]);
+
+        return {
+            statusCode: 201,
+            headers: {
+                "Access-Control-Allow-Origin": "*" // TODO: lock down to todobackend.com
+            },
         }
-    };
-
-    honeycombEvent.addField("latencyMs",Date.now()-startTime);
-    honeycombEvent.send();
-
-    return {
-        statusCode: 201,
-        headers: {
-            "Location": testRunUrl,
-            "Content-Type": "application/json;charset=utf-8",
-            "Access-Control-Allow-Origin": "*" // TODO: lock down to todobackend.com
-        },
-        body: JSON.stringify(response)
-    };
-};
-
-module.exports.recordResults = async (event,context) => {
-    const startTime = Date.now();
-    const honeycombEvent = honeycomb.newEvent();
-    honeycombEvent.add({
-        functionName: context.functionName,
-        functionVersion: context.functionVersion,
-        requestId: context.awsRequestId,
-    });
-
-    const testRunId = event.pathParameters.testRunId;
-    const completedAt = new Date().toISOString();
-
-    const testResults = event.body; // TODO: validation, DoS protection...
-    
-    honeycombEvent.add({
-        testRunId,
-        // TODO: full duration of test runs
-    });
-
-    await Promise.all([
-        recordRunCompletionInDb({uid:testRunId,completedAt}),
-        writeResultsToS3(testRunId,testResults)
-    ]);
-
-    honeycombEvent.addField("latencyMs",Date.now()-startTime);
-    honeycombEvent.send();
-
-    return {
-        statusCode: 201,
-        headers: {
-            "Access-Control-Allow-Origin": "*" // TODO: lock down to todobackend.com
-        },
-    };
-};
+      },
+      ctx.traceId, ctx.parentSpanId);
+}
 
 async function createTestRunInDb({uid,createdAt}){
     const item = {
@@ -113,12 +113,20 @@ async function recordRunCompletionInDb({uid,completedAt}){
 async function writeResultsToS3(uid,results){
     const key = `test-results/${uid}`;
 
+    const span = beeline.startSpan({
+        name: 'writeResultsToS3',
+        bucket: BUCKET_NAME,
+        key
+    });
+
     console.log('writing test results to',BUCKET_NAME,key);
     const result = await s3.putObject({
         Bucket: BUCKET_NAME,
         Key: key,
         Body: results
-    }).promise();
+    }).promise().finally( ()=> {
+        beeline.finishSpan(span);
+    })
 
     console.log('S3 put result:', result);
     return result;
